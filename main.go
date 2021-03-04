d8f0899c2dd8ff042e00b866f0028153142a960e
mohamed-mohaimen <mohamedveron23@gmail.com>
2021-03-04 03:01:40 +0200

    refactor code

--- main.go
+++ main.go
@@ -11,68 +11,6 @@ import (
 
 func main() {
 
-	/*f, err := os.Open("data_dump.csv")
-	if err != nil {
-		log.Fatal("Unable to read input file " + "data_dump.csv", err)
-	}
-	defer f.Close()
-
-	csvReader := csv.NewReader(f)
-	records, err := csvReader.ReadAll()
-	if err != nil {
-		log.Fatal("Unable to parse file as CSV for " + "data_dump.csv", err)
-	}
-
-	locations := []*domains.GeoLocation{}
-	ipMap := make(map[string]bool)
-
-	countValid, countInValid := 0,0
-
-	for idx, _ := range records {
-
-		gl := &domains.GeoLocation{}
-
-		valid := true
-
-		ipFlag , err := regexp.MatchString("\\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b", records[idx][0])
-		codeFlag, err := regexp.MatchString("^[a-zA-Z]+$", records[idx][1])
-		countryFlag, err := regexp.MatchString("^[a-zA-Z]+$", records[idx][2])
-		cityFlag, err := regexp.MatchString("^[a-zA-Z]+$", records[idx][3])
-
-		if !ipFlag || !codeFlag || !countryFlag || !cityFlag{
-			//fmt.Println("here str", codeFlag, countryFlag, cityFlag)
-			valid = false
-		}
-		if gl.Latitude, err = strconv.ParseFloat(records[idx][4], 64); err != nil {
-			valid = false
-		}
-		if gl.Longitude, err = strconv.ParseFloat(records[idx][5], 64); err != nil {
-			valid = false
-		}
-
-		if gl.MysteryValue, err = strconv.ParseInt(records[idx][6], 10,64); err != nil {
-			valid = false
-		}
-
-		if _, ok := ipMap[records[idx][0]]; !ok && valid {
-
-			gl.IpAddress = records[idx][0]
-			gl.CountryCode =  records[idx][1]
-			gl.Country =      records[idx][2]
-			gl.City =     records[idx][3]
-
-			locations = append(locations, gl)
-
-			ipMap[records[idx][0]] = true
-
-			countValid++
-		}else{
-			countInValid++
-		}
-
-	}
-	fmt.Println(countInValid, " : ", countValid , " :")*/
-
 	dbSettings := persistence.DBSettings{
 		Host:     "localhost",
 		Port:     5432,
@@ -85,7 +23,10 @@ func main() {
 	persistenceLayer := persistence.NewPersistence(&dbSettings)
 	serviceLayer := service.NewService(persistenceLayer, "data_dump.csv")
 
-	serviceLayer.DataIngestor()
+	// start the importing process
+
+	numberOfGoroutines := 10
+	serviceLayer.RunDataIngestor(numberOfGoroutines)
 	server := api.NewServer(serviceLayer)
 
 	// prepare router
--- service/data_ingestor.go
+++ service/data_ingestor.go
@@ -7,18 +7,19 @@ import (
 	"time"
 )
 
-func (s *Service) DataIngestor() (int, int, error) {
+func (s *Service) RunDataIngestor(numberOfGoroutines int) (int, int, time.Duration, error) {
 
-	locations := []*domains.GeoLocation{}
 	countValid, countInValid := 0,0
 
 	// pipeline of data ingestion
 	importedData := s.ReadCsvData();
 
+	// save locations to channel for concurrent access
 	ch := make(chan domains.GeoLocation)
 
 	errg := errgroup.Group{}
-	for i := 0; i < 10; i++ {
+
+	for i := 0; i < numberOfGoroutines; i++ {
 		errg.Go(func() error {
 			return s.worker(ch)
 		})
@@ -36,15 +37,12 @@ func (s *Service) DataIngestor() (int, int, error) {
 			gl.Country =      importedData[idx][2]
 			gl.City =     importedData[idx][3]
 
-			locations = append(locations, gl)
-
 			s.ipMap[importedData[idx][0]] = true
 
 			countValid++
 
-			if len(locations) < 180000 {
-				ch <- *gl
-			}
+			// add valid location to the channel
+			ch <- *gl
 
 		}else{
 			countInValid++
@@ -56,30 +54,12 @@ func (s *Service) DataIngestor() (int, int, error) {
 	err := errg.Wait()
 	if err != nil {
 		e := "Error while persist locations "
-		return countValid, countInValid, fmt.Errorf("%s: %s", e, err)
+		return countValid, countInValid, time.Since(startTime), fmt.Errorf("%s: %s", e, err)
 	}
 
 	fmt.Println(countInValid, " : ", countValid , time.Since(startTime) )
 
-	return countValid, countInValid, nil
-}
-
-func (s *Service) insertValidLocationsToDB(locations []*domains.GeoLocation) error {
-
-	for idx, _ := range locations {
-
-		err := s.persistence.CreateGeoLocation(locations[idx])
-
-		if err != nil {
-			return err
-		}
-
-		if idx >1000 {
-			break
-		}
-	}
-
-	return nil
+	return countValid, countInValid, time.Since(startTime), nil
 }
 
 func (s *Service) worker(locations chan domains.GeoLocation) error {
